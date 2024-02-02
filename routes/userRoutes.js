4ec5c2b781d919786b2f4274f7260f0c21ff1798
kalanishivam <kalanishivam@gmail.com>
2024-02-02 20:30:10 +0530

    feat : added APIs

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import errorHandler from '../middlewares/errorHandler.js';
 import { upload } from '../middlewares/multer.js';
-import { createProfile, getProfile, updateProfile } from '../controllers/userController.js';
+import { createProfile, fetchAllUsers, fetchMyReferral, getProfile, updateProfile } from '../controllers/userController.js';
 import { buyPackage,fetchPackage } from '../controllers/packageController.js';
 import { buySlot, fetchslot } from '../controllers/slotController.js';
 const router = express.Router();
@@ -17,6 +17,14 @@ router.post('/fetchPackages',errorHandler,fetchPackage);
 //slot Routers
 router.post('/buySlots',errorHandler,buySlot);
 router.post('/fetchSlots',errorHandler,fetchslot)
+
+
+// user Section from frontend routes
+// router.get('/allusers/:startDate?/:endData?' , fetchAllUsers)
+router.get('/allUsers' , fetchAllUsers)      // Note : This route is using query of start and end date
+
+// router.get('/referrals/:address/:startDate?/:endDate?' , fetchMyReferral)
+router.get('/referrals/:address' , fetchMyReferral)  // Note : This route is using query of start and end date
 
 
 export default router;
