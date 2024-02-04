6190d0856bd139427a0630a341208f56a35d5926
kalanishivam <kalanishivam@gmail.com>
2024-02-04 19:02:03 +0530

    feat : added API

--- controllers/activityController.js
+++ controllers/activityController.js
@@ -1,4 +1,5 @@
 import activities from "../models/activity.js";
+import users from "../models/users.js";
 
 
 export const fetchAllActivities = async(req, res)=>{
@@ -11,6 +12,35 @@ export const fetchAllActivities = async(req, res)=>{
 
     }catch(error){
         console.log(`error in fetching all activities in controller : ${error.message}`);
+        return res.status(500).json({error : "Internal server error"})
+    }
+}
+
+
+export const getDashboardInfo = async(req, res)=>{
+    try{
+        const {address} = req.params;
+        if(!address){
+            return res.status(400).json({error : "Please provide the address."})
+        }
+        const exists = await users.findOne({address : address});
+        if(!exists){
+            return res.status(400).json({error : "No such user exists."});
+        }
+        const totalMembers = await users.countDocuments();
+        let data = {
+         totalTeam: exists.referTo.length,
+         totalIncome : exists.packageIncome + exists.slotIncome + exists.levelIncome + exists.refferalIncome,
+         refferalIncome : exists.refferalIncome,
+         packageIncome : exists.packageIncome,
+         slotIncome : exists.slotIncome,
+         levelIncome : exists.levelIncome,
+         totalMembers : totalMembers 
+        }
+        return res.status(200).json({data});
+
+    }catch(error){
+        console.log(`error in fetching profile from user : ${error.message} `)
         return res.status(500).json({error : "Internal server error"})
     }
 }
--- routes/activityRoutes.js
+++ routes/activityRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import { fetchAllActivities } from '../controllers/activityController.js';
+import { fetchAllActivities, getDashboardInfo } from '../controllers/activityController.js';
+// import { getDashboardInfo } from '../controllers/dashboardController.js';
 // import { fetchAllActivities } from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.get('/all' , fetchAllActivities);
+router.get('/dashboard/:address' ,getDashboardInfo)
 
 export default router;
